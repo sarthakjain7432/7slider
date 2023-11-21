1ded846efc6958c6ad71fd5d5259893f71ba4961
sarthakjain7432 <69512822+sarthakjain7432@users.noreply.github.com>
2023-11-21 16:05:50 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import people from "./data";
 
 function App() {
+  const [state,setState] = useState(people);
+  const [index,setIndex] = useState(0);
+  const {id,image,name,title,quote} = state[index]; 
+ 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (index >= state.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    }, 3000);
+  return () => clearTimeout(timeoutId);
+  }, [index]);
+
+  const previous=()=>{
+    if(index <= 0){
+      setIndex(state.length - 1)
+    }
+    else {
+      setIndex(index - 1)
+    }
+  }
+  
+  const next=()=>{
+    if(index >= state.length - 1){
+      setIndex(0)
+    }
+    else {
+      setIndex(index + 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div style={{"height":"400px","width":"600px","backgroundColor":"lightblue","margin":"auto","marginTop":"100px","textAlign":"center"}}>
+        <>
+        
+          <img src={image} alt="" style={{"height":"150px","width":"150px","borderRadius":"50%","marginTop":"20px"}}/>
+          <div >❞</div>
+        
+          <div style={{"fontSize":"30px"}}>{name}</div>
+          <div>{title}</div>
+          <p>{quote}</p>
+          <button onClick={previous}>previous</button>
+          <button onClick={next}>next</button>
+        </>
     </div>
+    </>
   );
 }
 
